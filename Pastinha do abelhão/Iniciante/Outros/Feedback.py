1e5fe8e569e2c80be12760adf47b26cdd734ed80
André Luís <167043436+kanum6@users.noreply.github.com>
2024-12-19 21:26:23 -0400

    Add files via upload

---
+++ Pastinha do abelhão/Iniciante/Outros/Feedback.py
@@ -0,0 +1,16 @@
+cs = int(input())
+for i1 in range (cs):
+    cs1 = int(input())
+    for i2 in range (cs1):
+        k = int(input())
+        if k == 1:
+            print ("Rolien")
+            
+        elif k == 2:
+            print ("Naej")
+
+        elif k == 3:
+            print ("Elehcim")
+
+        elif k == 4:
+            print ("Odranoel")
